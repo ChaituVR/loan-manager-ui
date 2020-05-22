34ae159ed1ec575143044bd7392cbb4a433a77f5
Chaitanya Raju <yourchaitu@gmail.com>
2020-05-22 21:44:28 +0530

    Fix

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,7 +5,15 @@ import 'firebase/firestore';
 import 'firebase/database';
 
 var firebaseConfig = {
-  // get values from https://i.imgur.com/FyMSlhm.jpg
+  apiKey: 'AIzaSyCH31_WkQxtL0sPGgAyk2JmxRPhzX6_QHc',
+  authDomain: 'review-invitations.firebaseapp.com',
+  databaseURL: 'https://review-invitations.firebaseio.com',
+  projectId: 'review-invitations',
+  storageBucket: 'review-invitations.appspot.com',
+  messagingSenderId: '683577372550',
+  appId: '1:683577372550:web:5cdd93244c94add97b0607',
+  measurementId: 'G-4JNFVDE499',
+
 };
 
 firebase.initializeApp(firebaseConfig);
