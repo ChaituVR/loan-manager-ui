05f0fbca3c2c72ec124e29b9f8df2b855372b464
Chaitanya Raju <yourchaitu@gmail.com>
2020-05-22 23:26:49 +0530

    Fixes

--- src/components/Modules/Loans/AddNewLoan/AddLoan.form.js
+++ src/components/Modules/Loans/AddNewLoan/AddLoan.form.js
@@ -1,135 +1,78 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
-import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
 
-let id = 0;
+import { Form, Input, Button, Checkbox } from 'antd';
 
-class DynamicFieldSet extends React.Component {
-  remove = k => {
-    const { form } = this.props;
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys');
-    // We need at least one passenger
-    if (keys.length === 1) {
-      return;
-    }
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
-    // can use data-binding to set
-    form.setFieldsValue({
-      keys: keys.filter(key => key !== k),
-    });
+const AddLoanForm = () => {
+  const onFinish = values => {
+    console.log('Success:', values);
   };
 
-  add = () => {
-    const { form } = this.props;
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys');
-    const nextKeys = keys.concat(id++);
-    // can use data-binding to set
-    // important! notify form to detect changes
-    form.setFieldsValue({
-      keys: nextKeys,
-    });
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
   };
 
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   this.props.form.validateFields((err, values) => {
-  //     if (!err) {
-  //       const { keys, names } = values;
-  //       console.log('Received values of form: ', values);
-  //       console.log('Merged values:', keys.map(key => names[key]));
-  //     }
-  //   });
-  // };
-
-  render() {
-    const { getFieldDecorator, getFieldValue } = this.props.form;
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 6 },
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 18, offset: 2 },
-      },
-    };
-    const formItemLayoutWithOutLabel = {
-      wrapperCol: {
-        xs: { span: 24, offset: 0 },
-        sm: { span: 20, offset: 4 },
-      },
-    };
-    getFieldDecorator('keys', { initialValue: [] });
-    const keys = getFieldValue('keys');
-    const formItems = keys.map((k, index) => (
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
       <Form.Item
-        {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-        label={index === 0 ? 'Passengers' : ''}
-        required={false}
-        key={k}
-      >
-        {getFieldDecorator(`names[${k}]`, {
-          validateTrigger: ['onChange', 'onBlur'],
-          rules: [
-            {
-              required: true,
-              whitespace: true,
-              message: 'Please input passenger\'s name or delete this field.',
-            },
-          ],
-        })(<Input placeholder="passenger name" style={{ width: '60%', marginRight: 8 }} />)}
-        {keys.length > 1 ? (
-          <DeleteOutlined
-            className="dynamic-delete-button"
-            onClick={() => this.remove(k)}
-          />
-        ) : null}
-      </Form.Item>
-    ));
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Item label="Loan Name" {...formItemLayout}>
-          {getFieldDecorator('email', {
-            rules: [
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ],
-          })(<Input placeholder="Give your loan a name to easily identify" />)}
-        </Form.Item>
-        <Form.Item {...formItemLayout} label="Name">
+        label="Username"
+        name="username"
+        rules={[
           {
-            getFieldDecorator('username', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please input your name',
-                },
-              ],
-            })(<Input placeholder="Please input your name" />)}
-        </Form.Item>
-        {formItems}
-        <Form.Item {...formItemLayoutWithOutLabel}>
-          <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
-            <PlusOutlined /> Add field
-          </Button>
-        </Form.Item>
-        {/* <Form.Item {...formItemLayoutWithOutLabel}>
-          <Button type="primary" htmlType="submit">
-              Submit
-          </Button>
-        </Form.Item> */}
-      </Form>
-    );
-  }
-}
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-const AddLoanForm = Form.create({ name: 'dynamic_form_item' })(DynamicFieldSet);
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
 export default AddLoanForm;
