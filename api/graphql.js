01e5d4490c73e29ebe884da8c99baaae9752740e
Chaitanya Raju <yourchaitu@gmail.com>
2020-05-23 23:18:14 +0530

    Now Functions

---
+++ api/graphql.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+ 
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+ 
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+ 
+const server = new ApolloServer({ typeDefs, resolvers });
+ 
+const app = express();
+server.applyMiddleware({ app });
+
+// server.applyMiddleware({app, path: "/", cors: true});
+
+// app.listen({ port: 4000 }, () =>
+//   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+// );
+
+module.exports = app;
