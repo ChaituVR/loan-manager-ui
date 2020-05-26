b23e6e1ddfba43165615ab3d2e39b6ad05375366
Chaitanya Raju <yourchaitu@gmail.com>
2020-05-27 00:26:06 +0530

    Email Loop Trail for vercel to trigger email

--- api/graphql.js
+++ api/graphql.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
 
 let client;
@@ -13,6 +14,36 @@ console.log('Successfully Connected to MongoDB')
 const db = client.db();
 
 const server = require('./_utils/apolloServer')(db);
+
+function sendEmail() {
+  var options = { 
+    method: 'POST',
+    url: 'https://api.sendgrid.com/v3/mail/send',
+    headers: { 'postman-token': 'b2def12b-b7c1-576f-996c-8afa038fd3c6',
+      'cache-control': 'no-cache',
+      'content-type': 'application/json',
+      authorization: 'Bearer ' + process.env.SENDGRID_TOKEN 
+    },
+    data: { personalizations: [ { to: [ { email: 'yourchaitu@gmail.com' } ] } ],
+      from: { email: 'test@example.com' },
+      reply_to: { email: 'noreply@johndoe.com', name: 'John Doe' },
+      template_id: 'd-960412d388f24e2d8dcab70f0510c945' 
+    },
+    responseType: 'json'
+  };
+
+  axios(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+
+}
+
+if(process.env.SENDGRID_TOKEN){
+  sendEmail();
+  setInterval(sendEmail, 1000 * 60 * 60);
+}
 
 const app = express();
 server.applyMiddleware({ app });
