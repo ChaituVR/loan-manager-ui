cde2347ea9f6261085c5c393aaad856949e52522
Chaitanya Raju <yourchaitu@gmail.com>
2020-05-26 12:57:47 +0530

    Fix

--- src/components/Pages/Loans/index.container.js
+++ src/components/Pages/Loans/index.container.js
@@ -1,19 +1,11 @@
 import { connect } from 'react-redux';
-import { fetchCustomers } from './index.actions';
 import Customers from './index.component';
 
-const mapStateToProps = (state, props) => {
-  return {
-    products: state.products.data ? state.products.data : [],
-    loading: state.products.loading ? state.products.loading : false,
-    ...props,
-  };
-};
+// const mapStateToProps = (state, props) => {
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchCustomers: () => dispatch(fetchCustomers),
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Customers);
+// export default connect(mapStateToProps, mapDispatchToProps)(Customers);
+export default connect()(Customers);
