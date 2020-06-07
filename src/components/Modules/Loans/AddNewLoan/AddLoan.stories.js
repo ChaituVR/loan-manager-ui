12fd0bdce4a467905083cf8c4cc3e87b4c83ebca
chaituvr <chaituvr@CORP.AMAZONWORKSPACES.COM>
2020-06-07 11:47:11 +0530

    fixes

--- src/components/Modules/Loans/AddNewLoan/AddLoan.stories.js
+++ src/components/Modules/Loans/AddNewLoan/AddLoan.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import AddLoan from './AddLoan.component';
 import AddLoanForm from './AddLoan.form';
 
